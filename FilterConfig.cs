6dc5f681e502f73e06868d5101ebce502204eb20
NgoVanChien <chienngo03568@gmail.com>
2021-03-22 22:52:54 +0700

    first commit

---
+++ FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace NgoVanChien_5951071004
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
